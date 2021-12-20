57b628b4deec53033cd6761fbae79fecdee2bd99
LisaWhite-alt <laguna.crema@gmail.com>
2021-12-21 01:22:00 +0300

    learning function v.2

--- task_solution_function.R
+++ task_solution_function.R
@@ -11,3 +11,13 @@ decorate_string(pattern = "123", c("abc", "def"))
 decorate_string(pattern = "123", "abc", "def", sep = "+")
 decorate_string(pattern = "!", c("x", "x"), collapse = "_")
 decorate_string(pattern = ".:", 1:2, 3:4, 5:6, sep = "&")
+
+"""
+cat_temper <- c("задиристый", "игривый", "спокойный", "ленивый")
+cat_color <- c("белый", "серый", "чёрный", "рыжий")
+cat_age <- c("кот", "котёнок")
+cat_trait <- c("с умными глазами", "с острыми когтями", "с длинными усами")
+Составьте вектор cat_catalogue, содержащий всевозможные комбинации имеющихся характеристик, и отсортируйте его.
+"""
+cat_catalogue <- sort(outer(outer(outer(cat_temper, cat_color, paste), cat_age, paste), cat_trait, paste))
+cat_catalogue[42]
