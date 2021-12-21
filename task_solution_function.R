fa23643a3215ced326857a7a5fd8c6a4469fff55
LisaWhite-alt <laguna.crema@gmail.com>
2021-12-22 02:03:42 +0300

    learning function v.3

--- task_solution_function.R
+++ task_solution_function.R
@@ -12,6 +12,7 @@ decorate_string(pattern = "123", "abc", "def", sep = "+")
 decorate_string(pattern = "!", c("x", "x"), collapse = "_")
 decorate_string(pattern = ".:", 1:2, 3:4, 5:6, sep = "&")
 
+
 """
 cat_temper <- c("задиристый", "игривый", "спокойный", "ленивый")
 cat_color <- c("белый", "серый", "чёрный", "рыжий")
@@ -21,3 +22,47 @@ cat_trait <- c("с умными глазами", "с острыми когтями", "с длинными усами")
 """
 cat_catalogue <- sort(outer(outer(outer(cat_temper, cat_color, paste), cat_age, paste), cat_trait, paste))
 cat_catalogue[42]
+
+
+"""
+Блуждание по плоскости
+"""
+# Random walk with absorption
+simulate_walk <- function(lower = -6, upper = 6, n_max = 100, p = 0.01) {
+  current_position_x <- (lower + upper) / 2
+  current_position_y <- (lower + upper) / 2
+  for (i in 1:n_max) {
+    is_absorbed <- rbinom(1, 1, p)
+    if (is_absorbed) return(list(status = "Absorbed", 
+                                 position_x = current_position_x,
+                                 position_y = current_position_y,
+                                 steps = i))
+    current_position_x <- current_position_x + rnorm(1)
+    current_position_y <- current_position_y + rnorm(1)
+    distance <- sqrt(current_position_x^2 + current_position_y^2)
+    if (distance > 6) return(list(status = "Distance breach", 
+                                  position_x = current_position_x,
+                                  position_y = current_position_y,
+                                  steps = i))
+  }
+  return(list(status = "Max steps reached", 
+              position_x = current_position_x,
+              position_y = current_position_y,
+              steps = n_max))
+}
+
+# Simulate results
+result <- replicate(100000, simulate_walk(), simplify = FALSE)
+result <- data.frame(
+  status = sapply(result, function(x) x$status),
+  position_x = sapply(result, function(x) x$position_x),
+  position_y = sapply(result, function(x) x$position_y),
+  steps = sapply(result, function(x) x$steps)
+)
+
+# Inspect results
+r <- tapply(result$position_x, result$status, length)
+r
+r_mean <- r[2] / 100000 * 100
+r_mean
+
